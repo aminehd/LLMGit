662908dfecb5896cb006ff710cf5b7a9a252c008
Amineh Dadsetan <amineh.dadsetan@gmail.com>
2024-06-10 01:21:05 +0000

    Add Git utils

--- LLMGit/GitUtils/git.py
+++ LLMGit/GitUtils/git.py
@@ -1,8 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+from typing import List, Optional
+class Files:
+    path: str
+class Patch:
+    patch_raw_text: str
+    def get_changed_files(self) -> List[Optional[Files]]:
+        return []
+class GitCommit(BaseModel):
+    hash: str
+    author: str
+    email: str
+    date: str
+    message: str
+    patch: str
 
-class GitRepo(BaseModel):
-    name: str
-    url: str
 
-    def get_commit(self, commit: str) -> str:
-        return commit 
+class GitClient(BaseModel):
+    repo_url: HttpUrl
+    branchName: str
+    
+    def get_last_n_hash_codes(self, n: int) -> List[Optional[str]]:
+        return []
+    def get_last_n_commits(self, n: int) -> List[GitCommit]:
+        return []
+    def get_commit(self, hash: str) -> Optional[ GitCommit ]:
+        return None
--- tests/test_git.py
+++ tests/test_git.py
@@ -1,5 +1,34 @@
-from LLMGit.GitUtils import git
+from LLMGit.GitUtils.git import GitClient, GitCommit
 
-def test_evaluate_initialization():
-    repo = git.GitRepo(name='dummyname', url='dummy_url')
-    assert repo.name == 'dummyname'
+
+def test_get_last_n_commits():
+    # Instantiate GitRepo
+    git_client = GitClient(repo_url="https://github.com/django/django.git",branchName="main" )
+
+    # Call the method
+    commits = git_client.get_last_n_commits(1)
+    mock_patch = \
+        "diff --git a/example.py b/example.py\n" \
+        "index abcdef0..1234567 100644\n" \
+        "--- a/example.py\n" \
+        "+++ b/example.py\n" \
+        "@@ -1,3 +1,7 @@\n" \
+        " def existing_function():\n" \
+        "     print(\"This is an existing function\")\n" \
+        " \n" \
+        "+def new_function():\n" \
+        "+    print(\"This is a new function\")\n" \
+        "+\n" \
+        "+def another_new_function():\n" \
+        "+    print(\"This is another new function\")\n"
+
+    # Expected results
+    expected_commits = [
+        GitCommit(patch='mock_patch', hash="commit_hash_1", author="Author One", email="author1@example.com", date="Date One", message="Commit message one"),
+    ]
+
+    # Assertions
+    assert commits == expected_commits
+    assert len(commits) == 1
+    assert commits[0].patch.patch_raw_text == mock_patch
+    assert commits[0].pathc.get_changed_files() == '/acme/acme'
